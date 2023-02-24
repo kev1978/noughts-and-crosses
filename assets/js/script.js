0281aaefaf1e2a44eb71d72a833e332d3ec3ff3f
Kevin Cooksley <kevincooksley63@gmail.com>
2023-02-24 11:36:02 +0000

    added the play again option to start the game over again checked all lines of winning and works great

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ const strike =document.getElementById("strike");
 const gameOverArea = document.getElementById("game-over-area");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
+playAgain.addEventListener("click", startNewGame);
 
 
 const gameOverSound = new Audio("assets/sounds/mixkit-arcade-retro-game-over-213.wav");
@@ -70,9 +71,39 @@ function checkWinner(){
         const tileValue2 = boardState[combo[1]-1]
         const tileValue3 = boardState[combo[2]-1]
 
-        if (tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3)
+        if (tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3){
         strike.classList.add(strikeClass);
+        gameOverScreen(tileValue1);
+        return;
+        }
     }
+
+    const allTileFilledIn = boardState.every((tile) => tile !== null);
+    if (allTileFilledIn) {
+        gameOverScreen(null);
+    }
+
+}
+
+function gameOverScreen(winnerText) {
+    let text ='Draw!';
+    if(winnerText != null) {
+        text = `Winner is ${winnerText}!`;
+    }
+
+    gameOverArea.className = "visible";
+    gameOverText.innerText = text;
+    gameOverSound.play();
+}
+
+function startNewGame(){
+    strike.className = "strike";
+    gameOverArea.className = "hidden"
+    boardState.fill(null)
+    tiles.forEach((tile) => (tile.innerText = ""));
+    turn = PLAYER_X;
+    setHoverText();
+
 }
 
 const winningCombinations = [
@@ -85,6 +116,6 @@ const winningCombinations = [
     {combo:[2,5,8], strikeClass: "strike-column-2" },
     {combo:[3,6,9], strikeClass: "strike-column-3" },
 
-    {combo:[1,5,9], strikeClass: "strike-diagonal -1" },
-    {combo:[3,5,7], strikeClass: "strike-diagonal -2" },
+    {combo:[1,5,9], strikeClass: "strike-diagonal-1" },
+    {combo:[3,5,7], strikeClass: "strike-diagonal-2" },
 ];
